4029b30fc358ed3f6eff0f6cac4a1f734d04fd4d
bukyung <boochae2003@gmail.com>
2026-07-01 05:13:17 +0900

    feat: button, dropdown, input 공통 컴포넌트 구현

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,9 +1,24 @@
-const Button = () => {
-  return (
-    <div>
-      {/* TODO: Button */}
-    </div>
-  );
-};
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+  rounded?: "xl" | "full";
+  className?: string;
+}
 
-export default Button;
+// rounded 기본값 "xl" 로그인 버튼 스타일
+export default function Button({children, onClick, disabled, rounded = 'xl', className=''}: ButtonProps) {
+  const roundedStyle = rounded === "full" ? "rounded-full" : "rounded-xl"
+  console.log('button에 들어갈 텍스트:',children)
+  console.log('disabled 상태', disabled)
+
+  return (
+    <button 
+      className={`w-full h-[80px] text-[#FAFAFA] bg-[#FF8A3D] disabled:bg-[#DDDDDD] disabled:text-[#FAF8F3] disabled:cursor-not-allowed flex items-center justify-center ${roundedStyle} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
