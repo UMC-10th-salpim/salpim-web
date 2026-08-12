d5177d1843958acbab98ec2f4763f012b9c29ed9
YuSung Jung <joshuasa012@gmail.com>
2026-08-12 21:51:55 +0900

    fix: 로그인 비밀번호 버튼 문구 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -120,7 +120,7 @@ const LoginPage = () => {
               onClick={() => navigate('/password/find', { state: { phoneNumber: phone } })}
               className="min-h-12 rounded-full border-2 border-[#FFD29E] bg-white px-6 text-[24px] font-extrabold text-[#FF6B00] transition-colors hover:bg-[#FFF7ED]"
             >
-              비밀번호 찾기
+              비밀번호 바꾸기
             </button>
           </div>
 
